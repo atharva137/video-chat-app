d53524e1e99f411e8c9ced875f8ca0c2ee73b766
atharva137 <atharvayawalkar1377@gmail.com>
2021-07-06 02:41:52 +0530

    fix3

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ io.on("connection", (socket) => {
 	});
 });
 
-if(process.env.NODE_ENV === "production"){
-	app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === "production"){
+// 	app.use(express.static('client/build'));
 
-  app.get('*',(req,res)=>{
-	  res.sendFile(path.resolve(__dirname,'client','build','index.html')); 
-  })
-}
+//   app.get('*',(req,res)=>{
+// 	  res.sendFile(path.resolve(__dirname,'client','build','index.html')); 
+//   })
+// }
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
